7025600219eea49acf691ac231d23879cc4c6d33
Usuario <Usuario@192.168.0.124>
2016-10-13 10:35:44 -0600

    Actualizacion Main

--- src/com/ucr/proyecto/main/Main.java
+++ src/com/ucr/proyecto/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
         Empleado empleado = new Empleado("Jose", "asd32", 234567d, "I22334455", "Elver Galarga",1);
         Transaccion transaccion = new Transaccion(empleado, 23400d, "debitar");
         new Client(5700, Constantes.ENVIAR_TRANSACCION, transaccion).start();  
-        
+
     }
 
 }
